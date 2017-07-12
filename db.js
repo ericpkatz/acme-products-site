10d58d9151b89ce9b85963f8a79ace37c56dce4e
Eric P. Katz <ericpkatz@gmail.com>
2017-07-12 07:01:01 -0400

    fix post

--- db.js
+++ db.js
@@ -30,6 +30,13 @@ module.exports = {
     if(!product.name){
       throw 'product must have a name';
     }
+    var maxId = products.reduce(function(max, product){
+      if(product.id > max){
+        max = product.id;
+      }
+      return max;
+    }, 0);
+    product.id = ++maxId;
     products.push(product);
   },
   deleteProduct: function(id){
